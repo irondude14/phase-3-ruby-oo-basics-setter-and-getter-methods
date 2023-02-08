38182067115ff49f5767262df8140072ee0a83b6
Vladyslav <vkadyrov@protonmail.com>
2023-02-07 19:22:03 -0500

    commit tree 5308ba4df9d007d466a83d68799c1892b9c5a3de

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
+    
+    attr_accessor :name 
+    attr_accessor :breed
+    
+    def dog_name_breed(name, breed)
+        @name = name
+        @breed = breed
+    end
+
+    # def name
+    #     @name
+    # end
+
+    # def breed
+    #     @breed
+    # end
+
+    # def name=(new_name)
+    #     @name = new_name
+    # end
+
+    # def breed=(new_breed)
+    #     @breed = new_breed
+    # end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-
+    attr_accessor :name
+    attr_accessor :job
+    
+    def person_name_job(name, job)
+        @name = name
+        @job = job
+    end
 end
